f4718aa7d6a0991c62162af35738572c53b481df
Martin Moradi <moradi.martin@gmail.com>
2020-09-30 16:47:06 +0200

    Pyramid is working but with errors

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,19 +1,44 @@
-def ask_steps
-    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-    print "> "
-    steps = gets.chomp.to_i 
-    return steps
+def pyramid_height
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (choisis un nombre impair)"
+    height=gets.to_i
+    while !height.odd?
+        puts "Non, choisis un nombre impair !"
+        height=gets.to_i
+    end    
+    return height
 end
 
-def half_pyramid(steps)
-    i = steps - 1 
-    (1..steps).each do |num_step|
-        i.times {print " "}
-        num_step.times {print "#"} 
-        puts
-        i -= 1
+def pyramid_wtf(pyramid_height)
+    height = pyramid_height
+    (1..height).each do |row|
+        if row < height.to_f / 2
+            row_string = "#" * (2 * row - 1)                  
+        elsif row > height.to_f / 2           
+            row_string = "#" * ( (pyramid_height - row) * 2 - 1 )         
+        else
+             row_string = "#" * height
+        end
+        puts row_string.center(height) 
     end
 end
 
-steps = ask_steps
-half_pyramid(steps)
+
+
+pyramid_wtf(pyramid_height)
+
+
+
+=begin 
+height = pyramid_height
+width = pyramid_height 
+
+def full_pyramid(height)
+    (1..height).each do |row|
+    puts " " * ( height - row ) + "#" * (2 * row - 1)
+    end 
+end
+
+1) Demander le nombre d'étages
+2) Vérifier que le nombre est impair (while !odd?)
+3) Width = Height ? 
+=end
--- lib/test.rb
+++ lib/test.rb
@@ -1,7 +1,2 @@
-puts "Nombre d'étages de la pyramide ?"
-g=gets.to_i
-
-
-(1..g).each do |k|
- puts " " * (g-k) + "*" * (2*k-1)
-end 
+hello = "hello" * 3
+puts hello
